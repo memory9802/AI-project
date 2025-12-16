505f15f63d4c9da84a7335dd4e260746acd28b15
Julia Gao <memory9802@gmail.com>
2025-12-17 00:33:13 +0800

    items改用正確資料 & 補上user_wardrobe資料

--- init/00_init_with_data.sql
+++ init/00_init_with_data.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0cc66c592db0e46ac96ec78938e41c80b802ffe58ab6920c97f253869ffe62eb
-size 6496125
+oid sha256:2e7843f3e3eb0014ae0efbe2a02040e9db5d3e2f63f0be7a8e245d7a5aa43700
+size 5529238
